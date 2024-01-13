f6fc179f7fb12937da93cdc383aa5539173eb792
Felipeaugustobezerra <58082993+Felipeaugustobezerra@users.noreply.github.com>
2024-01-13 18:30:32 +0000

    add victory function

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,9 +1,8 @@
-// Constante contendo um array de emojis.
-const emojis = ["😀","😀","🤡","🤡","😈","😈","😭","😭","👨🏻‍💻","👨🏻‍💻","😱","😱","🤩","🤩","😍","😍"];
-// 
+// Const containing an array of emojis.
+const emojis = ["😀","😀","🤡","🤡","😈","😈","😭","😭","👨🏻‍💻","👨🏻‍💻","😱","😱","🤩","🤩","😍","😍"]; 
+//Save card positions
 let openCards = [];
-
-// Variavel para embaralhar os emojis. 
+//Variable to shuffle the emojis.  
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2: -1)); 
 
 // 
@@ -12,8 +11,42 @@ for(let i=0; i < emojis.length; i++){
     let box = document.createElement("div");
     //Create class item
     box.className = "item";
-    // change content class item 
+    // Change content class item 
     box.innerHTML = shuffleEmojis[i];
+    // 
+    box.onclick = handleClick;
     // Select class game and add variable box 
     document.querySelector(".game").appendChild(box)
+}
+// function for click
+function handleClick() {
+    // check if only 2 cards is open
+    if(openCards.length < 2 ){
+        this.classList.add("boxOpen");
+        //Ao clicar na carta salva nesse array
+        openCards.push(this);
+    }
+    //checks if a pair of cards are open
+    if(openCards.length == 2) {
+        setTimeout(checkMatch,500);
+    }
+}
+//function victory
+function checkMatch() {
+    //Check if the cards are the same
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        //if the cards are the same, add the BoxMatch class
+        openCards[0].classList.add("boxMatch")
+        openCards[1].classList.add("boxMatch")
+    }else{
+        //If they are not the same, remove the boxOpen class
+        openCards[0].classList.remove("boxOpen")
+        openCards[1].classList.remove("boxOpen")
+    }
+    //clearing the cards matrix vector
+    openCards = [];
+    //checks if all cards are open
+    if(document.querySelectorAll(".boxMatch").length == emojis.length){
+        alert("Voce Venceu !");
+    }
 }
